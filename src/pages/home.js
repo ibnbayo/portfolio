5d88a80953e64ecbfaacec81521a1e98bc848747
ibnbayo <abdullahifahm@gmail.com>
2022-07-25 11:57:37 +0100

    All done

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,33 +7,31 @@ import {
   HeaderWrapper,
   FooterWrapper,
 } from "../components/styles";
+import Favicon from "react-favicon";
 import {Preloader} from '../pageloader/index';
-import Logo from "../assets/logo.svg";
+import Logo from "../assets/aflogostr.png";
 import { ReactComponent as FooterLogo } from "../assets/logo.svg";
 import { ReactComponent as ResumeIcon } from "../assets/cv.svg";
 import { ReactComponent as LinkedinIcon } from "../assets/linkedin.svg";
 import { ReactComponent as GithubIcon } from "../assets/github.svg";
-import { ReactComponent as HashnodeIcon } from "../assets/hashnode.svg";
 import { ReactComponent as StackOverflowIcon } from "../assets/stack-overflow.svg";
 import { ReactComponent as EmailIcon } from "../assets/email.svg";
 import {ReactComponent as WebIcon} from '../assets/web.svg';
-import {ReactComponent as YTIcon} from '../assets/youtube.svg';
+import {ReactComponent as TwitterIcon} from '../assets/twittersvg.svg';
 import {ReactComponent as IGIcon} from '../assets/instagram.svg';
 import {ReactComponent as QuoraIcon} from '../assets/quora.svg';
-import {ReactComponent as PickyIcon} from '../assets/picky.svg';
+
 import HTMLIcon from "../assets/html.svg";
-import SassIcon from "../assets/sass.svg";
-import TWIcon from "../assets/tailwindcss.svg";
 import StyledIcon from "../assets/styled.png";
 import ReactIcon from "../assets/react.svg";
-import GatsbyIcon from "../assets/gatsby.svg";
-import NuxtIcon from "../assets/nuxt.svg";
-import VueIcon from "../assets/vue.svg";
-import GraphqlIcon from "../assets/graphql.svg";
 import SqlIcon from "../assets/sql.svg";
 import VscodeIcon from "../assets/vscode.svg";
 import GitIcon from "../assets/git.svg";
-import resume from '../assets/airahyusuff.pdf';
+import PythonIcon from "../assets/python.svg";
+import FlaskIcon from "../assets/flask.svg";
+import BootstrapIcon from "../assets/bootstrap.svg";
+import UbuntuIcon from "../assets/ubuntu.svg";
+
 
 export const findImg = (val, arr) => {
   const result = arr.find((el) => el.public_id.includes(val) === true)
@@ -44,41 +42,27 @@ const tools = [
     url: "https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics",
     logo: HTMLIcon
   },
-  {
-    url: "https://sass-lang.com/",
-    logo: SassIcon
-  },
-  {
-    url: "https://tailwindcss.com/",
-    logo: TWIcon
-  },
+
   {
     url: "https://styled-components.com/",
     logo: StyledIcon
   },
   {
+    url: "https://getbootstrap.com/",
+    logo: BootstrapIcon
+  },
+  {
     url: "https://reactjs.org/",
     logo: ReactIcon
   },
-  {
-    url: "https://www.gatsbyjs.com/",
-    logo: GatsbyIcon
-  },
-  {
-    url: "https://nuxtjs.org/",
-    logo: NuxtIcon
-  },
-  {
-    url: "https://vuejs.org/",
-    logo: VueIcon
-  },
-  {
-    url: "https://graphql.org/",
-    logo: GraphqlIcon
-  },
+
   {
     url: "https://www.postgresql.org/",
     logo: SqlIcon
+  },
+  {
+    url: "https://ubuntu.com/",
+    logo: UbuntuIcon
   },
   {
     url: "https://code.visualstudio.com/",
@@ -88,53 +72,72 @@ const tools = [
     url: "https://git-scm.com/",
     logo: GitIcon
   },
+  {
+    url: "https://python.com/",
+    logo: PythonIcon
+  },
+  {
+    url: "https://flask.palletsprojects.com/en/2.1.x/",
+    logo: FlaskIcon
+  },
+ 
+ 
 ]
 const projects = [
   {
-    name: "Callerview Dashboard",
-    url: "https://callerview.netlify.app",
-    mark: "callerview"
+    name: "Meme Generator",
+    url: "https://ibnbayo.github.io/meme-app/",
+    mark: "React JS",
+    imgsrc: "https://i.postimg.cc/W38tG4mF/Screenshot-Buzz.png",
+    giturl: "https://github.com/ibnbayo/meme-app"
   },
   {
-    name: "AutiMS",
-    url: "https://autims.netlify.app",
-    mark: "autims"
+    name: "User Search",
+    url: "https://ibnbayo.github.io/User-Data-Search/",
+    mark: "Javascript",
+    imgsrc: "https://i.postimg.cc/J79tjSZQ/Screenshot-Users-Table.png",
+    giturl: "https://github.com/ibnbayo/User-Data-Search"
   },
   {
-    name: "Teume project",
-    url: "https://treasureproject.netlify.app",
-    mark: "treasure"
+    name: "Todo App",
+    url: "https://github.com/ibnbayo/fullstack-todo",
+    mark: "Python, Javascript, Postgres",
+    imgsrc: "https://i.postimg.cc/yY2nHxyx/Screenshot-FFPost.png",
+    giturl: "https://github.com/ibnbayo/fullstack-todo"
+  },
+
+  {
+    name: "Page Saver Chrome Extension",
+    url: "https://github.com/ibnbayo/page-saver",
+    mark: "Javascript",
+    imgsrc: "https://i.postimg.cc/HssHnrKm/Screenshot-page-saver.jpg",
+    giturl: "https://github.com/ibnbayo/page-saver"
   },
   {
-    name: "MCF",
-    url: "https://mentalcare.life",
-    mark: "mcf"
+    name: "Tesla Landing Page",
+    url: "https://ibnbayo.github.io/Tesla-Page/",
+    mark: "CSS",
+    imgsrc: "https://i.postimg.cc/x8RrC86Z/screenshot.jpg",
+    giturl: "https://github.com/ibnbayo/Tesla-Page"
   },
   {
-    name: "Fitila",
-    url: "https://fitila.mentalcare.life",
-    mark: "fitila"
+    name: "Toucan Restaurant",
+    url: "https://ibnbayo.github.io/v39-toucans-team-02/catering.html",
+    mark: "Bootstrap",
+    imgsrc: "https://i.postimg.cc/KcJ7DGJy/Screenshot-Restaurant.png",
+    giturl: "https://github.com/ibnbayo/v39-toucans-team-02"
   },
   {
-    name: "Filter Pay",
-    url: "https://filter-pay.netlify.app",
-    mark: "filter"
+    name: "Productivity App",
+    url: "https://ibnbayo.github.io/productivity-app/",
+    mark: "Javascript",
+    imgsrc: "https://i.postimg.cc/4xK68JRC/Screenshot-productivity.png",
+    giturl: "https://github.com/ibnbayo/productivity-app"
   },
-  {
-    name: "GitHub Repos",
-    url: "https://githubgraphql.netlify.app/",
-    mark: "graphql"
-  },
-  {
-    name: "MePaint",
-    url: "https://mepaint.netlify.app",
-    mark: "meyer"
-  },
-  {
-    name: "This Portfolio",
-    url: "#",
-    mark: "portfolio"
-  },
+
+
+
+ 
 ]
 
 const Home = () => {
@@ -161,21 +164,11 @@ const Home = () => {
       }
     }]
   };
-  const [allImages, setAllImages] = useState([]);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setCurrentYear(new Date().getFullYear());
-        axios.get('https://res.cloudinary.com/khairahscorner/image/list/project.json')
-        .then(res => {
-          setAllImages([...res.data.resources]);
-          setLoading(false);     
-        })
-  }, []);
 
   return (
-    loading ? Preloader : (
     <div>
+      <Favicon url="favicons.ico"></Favicon>
       <HeaderWrapper>
         <div className="logo">
           <NavLink to="/">
@@ -184,13 +177,13 @@ const Home = () => {
         </div>
         <div>
           <div className="resume-text">
-            <a href={resume} download="Airah-Yusuff-Resume.pdf">
-              Download My Resume
+            <a href="https://github.com/ibnbayo" target="_blank" rel="noreferrer">
+              Github
             </a>
           </div>
           <div className="resume-icon">
-            <a href={resume} download="Airah-Yusuff-Resume.pdf">
-              <ResumeIcon />
+            <a href="https://github.com/ibnbayo">
+              <GithubIcon />
             </a>
           </div>
         </div>
@@ -199,44 +192,28 @@ const Home = () => {
       <MainWrapper>
         <div className="first-section inner-content">
           <div className="intro">
-            <h1 className="intro-heading">Hi, I'm Airah Yusuff.</h1>
+            <h1 className="intro-heading">Hi, I'm Abdullahi Fahm.</h1>
             <p className="intro-desc">
-              Frontend Engineer &#x2022; Technical Writer
+              Frontend Engineer &#x2022; Aspiring Fullstack Engineer
             </p>
             <div className="separator"></div>
             <div className="social-icons">
+              
               <div className="icon">
                 <a
                   target="_blank"
                   rel="noreferrer"
-                  href="https://www.linkedin.com/in/khairah-yusuff/"
-                >
-                  <LinkedinIcon />
-                </a>
-              </div>
-              <div className="icon">
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://github.com/khairahscorner"
+                  href="https://github.com/ibnbayo"
                 >
                   <GithubIcon />
                 </a>
               </div>
+              
               <div className="icon">
                 <a
                   target="_blank"
                   rel="noreferrer"
-                  href="https://khairahscorner.hashnode.dev/"
-                >
-                  <HashnodeIcon />
-                </a>
-              </div>
-              <div className="icon">
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://stackoverflow.com/users/11233049/airah-yusuff?tab=answers"
+                  href="https://stackoverflow.com/users/19413350/abdullahi"
                 >
                   <StackOverflowIcon />
                 </a>
@@ -245,7 +222,16 @@ const Home = () => {
                 <a
                   target="_blank"
                   rel="noreferrer"
-                  href="mailto:airahyusuff@gmail.com?subject=From Your Portfolio"
+                  href="https://twitter.com/zzinedddine"
+                >
+                  <TwitterIcon />
+                </a>
+              </div>
+              <div className="icon">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="mailto:abdullahifahm@gmail.com?subject=From Your Portfolio"
                 >
                   <EmailIcon />
                 </a>
@@ -256,18 +242,17 @@ const Home = () => {
         <div className="second-section">
           <div className="inner-content inner-padding">
             <div className="section-wrapper">
-              <h2 className="section-heading">Brief Intro.</h2>
+              <h2 className="section-heading">Profile</h2>
               <div className="bio">
                 <p>
-                  I'm a Frontend Engineer with 3+ years of experience developing
+                  I'm a Frontend Engineer with experience in developing
                   visually appealing web applications with delightful user
-                  experience.{" "}
+                  experiences.{" "}
                 </p>
                 <p>
-                  As an avid learner, I thrive in work environments that provide
-                  measurable metrics for career growth, great opportunities for
-                  skills improvement and promotes close team collaborations and
-                  soft skills acquisition.
+                I have a heightened sense of curiosity about software,
+                 developer tools, and the application of technical concepts 
+                 to solving problems. 
                 </p>
               </div>
             </div>
@@ -290,23 +275,31 @@ const Home = () => {
         <div className="third-section">
         <div className="inner-content inner-padding">
             <div>
-              <h2 className="section-heading">Past Projects.</h2>
+              <h2 className="section-heading">Projects</h2>
               <div className="flex-row">
                   {projects.map((project, index) => (
                     <div className="member" key={`project-${index}`}>
                       <div className="inner">
                       <div className="img-box">
-                        <img src={`https://res.cloudinary.com/khairahscorner/image/upload/${findImg(project.mark, allImages)}.jpg`} alt={project.name} />
+                        <img src={project.imgsrc} alt={project.name} />
                         <div className="overlay"/>
                       </div>
                       <div className="content">
                         <div className="text">
                           {project.name}
                         </div>
-                        <a className="btn" href={project.url} target="_blank" rel="noreferrer"> 
+                        <div className="webgit">
+                        <a className="btn" id="weblink" href={project.url} target="_blank" rel="noreferrer"> 
                           <WebIcon />
                         </a>
+                        <a className="btn" id="gitlink" href={project.giturl} target="_blank" rel="noreferrer"> 
+                          <GithubIcon />
+                        </a>
+                        </div>
                       </div>
+                      <div className="subtext">
+                          {project.mark}
+                        </div>
                       </div>
                     </div>
                   ))}      
@@ -317,69 +310,27 @@ const Home = () => {
         <div className="second-section">
           <div className="inner-content inner-padding">
             <div className="section-wrapper">
-              <h2 className="section-heading">Outside of Work.</h2>
+              <h2 className="section-heading">About</h2>
               <div className="bio mb-20">
                 <p>
-                I create modest fashion, lifestyle and skincare content for my blog and different social media platforms!🥰{" "}
+                A software engineering professional with entry-level experience specializing in software development with
+                 Python and JavaScript.  {" "}
                 </p>
-                <p>I also share general helpful tips, blogging resources and technical articles.</p>
+                <p>
+                I am a creative, self-motivated and passionate Front-End Developer, passionate about writing clean and organized code, 
+                and keen on personal and professional development.
+
+            I offer my ability (fueled by my passion for building solutions) to deliver creative, elegant and efficient solutions 
+            to problems. I always seek to improve my skillset and stay on top of latest technologies.  {" "}
+                </p>
+                <p>When not coding, I'm either reading or watching videos on technology and 
+                  it's role in making the world a better place.
+
+                I'm also very ethusiastic about Space exploration, environmental sustainability and Artificial Intelligence.
+</p>
+              
               </div>
-              <div className="social-icons">
-              <div className="round-icon">
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://www.khairahscorner.com"
-                >
-                  <WebIcon />
-                </a>
-              </div>
-              <div className="round-icon">
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://youtube.com/khairahscorner"
-                >
-                  <YTIcon />
-                </a>
-              </div>
-              <div className="round-icon">
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://instagram.com/khairahscorner"
-                >
-                  <IGIcon />
-                </a>
-              </div>
-              <div className="round-icon">
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://www.quora.com/profile/Airah-Yusuff"
-                >
-                  <QuoraIcon />
-                </a>
-              </div>
-              <div className="round-icon">
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://pinterest.com/khairahscorner"
-                >
-                
-                </a>
-              </div>
-              <div className="round-icon">
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://www.gopicky.com/skincarebiased"
-                >
-                  <PickyIcon />
-                </a>
-              </div>
-            </div>
+             
             </div>
           </div>
         </div>
@@ -388,12 +339,12 @@ const Home = () => {
       <FooterWrapper>
         
         <div className="credit">
-          © {currentYear} 
+          © 2022 
         </div>
       </FooterWrapper>
     </div>
     )
-  );
+
 };
 
 export default Home;
